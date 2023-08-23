18165bc42abff6f046824053176afe4d8f714b63
Kaushik Dey <Kaushikdey647@gmail.com>
2023-08-23 11:34:20 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-debug = True
-host = 'localhost'
-port = 5000
-mongo_uri = 'mongodb+srv://superadmin:sastapassword@cluster-1.xy3x22u.mongodb.net/?retryWrites=true&w=majority'
-db_name = 'frg-db'
-hf_api_url = "https://api-inference.huggingface.co/models/meta-llama/Llama-2-7b-chat-hf"
-hf_headers = {"Authorization": "Bearer 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"}
